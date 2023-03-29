354971cffca3d88cb1124efeaecfe9f6d764afa3
DinoChan-SuiteSpot <dino.chan@suitespottechnology.com>
2023-03-29 09:14:58 -0400

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { aws_lambda as lambda, aws_logs as logs } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as chime from 'cdk-amazon-chime-resources';
 import { Construct } from 'constructs';
 import path from 'path';
 import url from 'url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-export class IvrWithChimeSdk extends cdk.Stack {
+const ID = 'poc-ivr-twilio';
+
+export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
-    super(scope, id);
+    super(scope, id, props);
 
     const func = new NodejsFunction(this, 'NodejsFunction', {
+      functionName: ID,
       entry: path.join(__dirname, 'functions/index.ts'),
       depsLockFilePath: path.join(__dirname, 'functions/pnpm-lock.yaml'),
       runtime: lambda.Runtime.NODEJS_18_X,
       timeout: cdk.Duration.seconds(60),
-    });
-    func.addToRolePolicy(
-      new iam.PolicyStatement({
-        actions: ['chime:*'],
-        resources: ['*'],
-      })
-    );
-
-    const pstnPhoneNumber = new chime.ChimePhoneNumber(this, 'chime.ChimePhoneNumber', {
-      phoneCountry: chime.PhoneCountry.CA,
-      phoneProductType: chime.PhoneProductType.SMA,
-      phoneNumberType: chime.PhoneNumberType.LOCAL,
+      logRetention: logs.RetentionDays.ONE_WEEK,
+      reservedConcurrentExecutions: 1,
     });
 
-    const sipMediaApp = new chime.ChimeSipMediaApp(this, 'chime.ChimeSipMediaApp', {
-      endpoint: func.functionArn,
-    });
-
-    new chime.ChimeSipRule(this, 'chime.ChimeSipRule', {
-      triggerType: chime.TriggerType.TO_PHONE_NUMBER,
-      triggerValue: pstnPhoneNumber.phoneNumber,
-      targetApplications: [
-        {
-          priority: 1,
-          sipMediaApplicationId: sipMediaApp.sipMediaAppId,
-        },
-      ],
-    });
-
-    new cdk.CfnOutput(this, 'pstnPhoneNumber', {
-      value: pstnPhoneNumber.phoneNumber,
+    new lambda.FunctionUrl(this, ' lambda.FunctionUrl', {
+      function: func,
+      authType: lambda.FunctionUrlAuthType.NONE,
     });
   }
 }
 
 const app = new cdk.App();
-new IvrWithChimeSdk(app, IvrWithChimeSdk.name, {
+new Stack(app, ID, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,135 +1,63 @@
-import { Handler } from 'aws-lambda';
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import twilio from 'twilio';
 
-export const handler: Handler = async (event, context) => {
-  console.log(JSON.stringify(event));
+const callSid2data = new Map<string, { tree: string[]; hangup: boolean }>();
 
-  const callId: string = event.CallDetails?.Participants?.[0]?.CallId;
-  let tree: string = event.CallDetails?.TransactionAttributes?.tree;
-  let actions: Record<string, unknown>[];
-  switch (event.InvocationEventType) {
-    case 'NEW_INBOUND_CALL': {
-      actions = [speakAndGetDigits({ callId: callId, text: 'Press 1 for 1\nPress 2 for 2\nPress 3 for 3' })];
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  console.log(JSON.stringify(event, undefined, 2));
+  if (event.body === undefined) throw new Error('No body');
+  const body = new URLSearchParams(event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString() : event.body);
+  console.log(body);
+
+  const callSid = body.get('CallSid')!;
+
+  const voiceRes = new twilio.twiml.VoiceResponse();
+
+  switch (body.get('CallStatus')) {
+    case 'ringing': {
+      callSid2data.set(callSid, { tree: [], hangup: false });
+      const gather = voiceRes.gather({ numDigits: 1, actionOnEmptyResult: true });
+      gather.say('Press 1 for 1\nPress 2 for 2\nPress 3 for 3');
+      voiceRes.redirect('/');
       break;
     }
-    case 'RINGING': {
-      actions = [];
-      break;
-    }
-    case 'HANGUP': {
-      actions = [];
-      break;
-    }
-    case 'ACTION_SUCCESSFUL': {
-      switch (event.ActionData.Type) {
-        case 'SpeakAndGetDigits': {
-          const digit = event.ActionData.ReceivedDigits;
-          tree = tree === undefined ? digit : `${tree}.${digit}`;
-          if (tree === '3.3.3') {
-            actions = [speak({ callId, text: `Start recording for 6 seconds after Bee, press any key to stop. Bee` })];
-          } else if (['1', '2', '3'].includes(digit)) {
-            actions = [
-              speakAndGetDigits({
-                callId: callId,
-                text: Array(3)
-                  .fill(undefined)
-                  .map((v, i) => `Press ${i + 1} for ${tree}.${i + 1}`)
-                  .join('\n'),
-              }),
-            ];
-          } else {
-            actions = [hangup({ callId })];
-          }
-          break;
+    case 'in-progress': {
+      const data = callSid2data.get(callSid)!;
+      if (data.hangup) {
+        voiceRes.hangup();
+      } else {
+        const digit = body.get('Digits')!;
+        if (['1', '2', '3'].includes(digit)) data.tree.push(digit);
+        if (data.tree.length === 3 && data.tree.every((node) => node === '3')) {
+          voiceRes.say('Start recording for 6 seconds after Bee, press # key to stop.');
+          voiceRes.record({ maxLength: 6, finishOnKey: '#', playBeep: true });
+          data.hangup = true;
+        } else {
+          const prefix = data.tree.join('.');
+          const gather = voiceRes.gather({ numDigits: 1, actionOnEmptyResult: true });
+          gather.say(`Press 1 for ${prefix}.1\nPress 2 for ${prefix}.2\nPress 3 for ${prefix}.3`);
         }
-        case 'Speak': {
-          if (tree === '3.3.3') {
-            actions = [recordAudio({ callId: callId })];
-          } else {
-            actions = [hangup({ callId })];
-          }
-          break;
-        }
-        case 'RecordAudio': {
-          const objectKey = event.ActionData.RecordingDestination.Key;
-          actions = [speak({ callId, text: objectKey })];
-          break;
-        }
-        default:
-          console.warn(`Unhandled Action Type: ${event.ActionData.Type}`);
-          actions = [hangup({ callId })];
-          break;
       }
+      voiceRes.redirect('/');
       break;
     }
-    case 'ACTION_FAILED': {
-      actions = [speak({ callId, text: 'ABC' }), hangup({ callId })];
+    case 'completed': {
+      callSid2data.delete(callSid);
       break;
     }
-    default:
-      actions = [
-        speak({ callId, text: `Unhandled Invocation Event Type: ${event.InvocationEventType}` }),
-        hangup({ callId }),
-      ];
+    default: {
+      throw new Error('Unhandled CallStatus: ' + body.get('CallStatus'));
+    }
   }
-  const response = {
-    SchemaVersion: '1.0',
-    Actions: actions,
-    TransactionAttributes: { tree },
+
+  const xml = voiceRes.toString();
+  console.log(xml);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+    body: xml,
   };
-  console.log(JSON.stringify(response));
-  return response;
 };
-
-function speakAndGetDigits(arg: { callId: string; text: string }) {
-  return {
-    Type: 'SpeakAndGetDigits' as const,
-    Parameters: {
-      CallId: arg.callId,
-      SpeechParameters: {
-        Text: arg.text,
-      },
-      FailureSpeechParameters: {
-        Text: 'Timeout',
-      },
-      MinNumberOfDigits: 1,
-      MaxNumberOfDigits: 1,
-      RepeatDurationInMilliseconds: 10000,
-    },
-  };
-}
-
-function speak(arg: { callId: string; text: string }) {
-  return {
-    Type: 'Speak' as const,
-    Parameters: {
-      CallId: arg.callId,
-      Text: arg.text,
-    },
-  };
-}
-
-function hangup(arg: { callId: string }) {
-  return {
-    Type: 'Hangup' as const,
-    Parameters: {
-      CallId: arg.callId,
-      SipResponseCode: '0',
-    },
-  };
-}
-
-function recordAudio(arg: { callId: string }) {
-  return {
-    Type: 'RecordAudio' as const,
-    Parameters: {
-      CallId: arg.callId,
-      DurationInSeconds: '6',
-      RecordingTerminators: Array.from('123456789*0#'),
-      RecordingDestination: {
-        Type: 'S3' as const,
-        BucketName: 'new-audio-bucket-name-905418194649',
-        Prefix: 'recordings/',
-      },
-    },
-  };
-}
